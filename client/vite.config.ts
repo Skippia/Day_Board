500458ae445ecc83a9130b76d536ea709bd83f8f
Lormida <pocketbook.love24@gmail.com>
2022-07-08 00:36:09 +0300

    feat: Add date picker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,12 +2,12 @@
 
 import path from 'path'
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import UnoCSS from 'unocss/vite'
-import { presetAttributify, presetUno } from 'unocss'
+import { presetAttributify, presetIcons, presetUno } from 'unocss'
 import transformerDirective from '@unocss/transformer-directives'
 
 export default defineConfig({
@@ -17,29 +17,12 @@ export default defineConfig({
     },
   },
   plugins: [
-    Vue({
+    vue({
       reactivityTransform: true,
     }),
 
     // https://github.com/hannoeru/vite-plugin-pages
     Pages(),
-
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        // 'vue/macros',
-        'vue-router',
-      ],
-      dts: true,
-      dirs: [
-        './src/stores',
-        './src/utils',
-        './src/hooks',
-      ],
-      vueTemplate: true,
-    }),
-
     // https://github.com/antfu/vite-plugin-components
     Components({
       dirs: [
@@ -53,6 +36,21 @@ export default defineConfig({
       ],
       dts: true,
     }),
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        'vue/macros',
+        'vue-router',
+      ],
+      dts: true,
+      dirs: [
+        './src/stores',
+        './src/utils',
+        './src/hooks',
+      ],
+      vueTemplate: true,
+    }),
 
     // https://github.com/antfu/unocss
     // see unocss.config.ts for config
@@ -60,11 +58,23 @@ export default defineConfig({
       presets: [
         presetUno(),
         presetAttributify(),
+        presetIcons({
+          customizations: {
+            transform(svg) {
+              return svg.replace(/#fff/, '#fff')
+            },
+          },
+          extraProperties: {
+            'display': 'inline-block',
+            'vertical-align': 'middle',
+          },
+        }),
       ],
       transformers: [
         transformerDirective(),
       ],
     }),
+
   ],
 
   // https://github.com/vitest-dev/vitest
