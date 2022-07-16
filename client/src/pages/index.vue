6fcffde08df1cdfedeff688c3b139523d07694a6
Lormida <pocketbook.love24@gmail.com>
2022-07-16 15:39:27 +0300

    chore: update eslint & prettier config

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,34 +1,35 @@
 <script setup lang="ts">
-import { useModal } from '~/logic/useModal'
-
-const StoreUser = useStoreUser()
-// @ts-expect-error  ???
-const searcherTds = () => [...document.querySelectorAll('td')].filter(el => !el.classList.contains('time'))
-const searcherTable = () => document.querySelector('table')
-const isTableCell = UTIL.createSelector({ tagName: 'td' })
+    import { useModal } from '~/logic/useModal'
 
     const StoreUser = useStoreUser()
-
     const searcherTds = () => [...document.querySelectorAll('td')].filter((el) => !el.classList.contains('time'))
     const searcherTable = () => document.querySelector('table')
     const isTableCell = UTIL.createSelector({ tagName: 'td' })
 
     const { yMapper, xMapper, times, tasks, currentList } = useInitCrossTableValues(StoreUser)
 
-    const { generateColor } = useGenerateColor({
-        rangeTransparency: [0, 70],
-        colorTransparency: [0, 30],
-    })
+    const { generateColor } = useGenerateColor({ rangeTransparency: [0, 70], colorTransparency: [0, 30] })
 
     //* To paint table cells by selector
     usePaintTableCell({ searcher: searcherTds, generateColor })
 
-onMounted(() => {
-  //* Upload data
-  StoreUser.restoreCurrentDay(currentList)
-})
+    //* Get click listener
+    const { clickActionOnTableCell } = useTableClickAction({
+        isClickElement: isTableCell,
+        generateColor,
+        updateCurrentDay: StoreUser.updateCurrentDay,
+        currentList,
+    })
 
-const { isOpen, openModal, closeModal } = useModal()
+    //* Get hover listener
+    const { hoverActionOnTableCell } = useTableHoverAction({ searcherTable, isHoverElement: isTableCell })
+
+    onMounted(() => {
+        //* Upload data
+        StoreUser.restoreCurrentDay(currentList)
+    })
+
+    const { isOpen, openModal, closeModal } = useModal()
 </script>
 
 <template>
@@ -56,28 +57,24 @@ const { isOpen, openModal, closeModal } = useModal()
             </template>
         </CrossTable>
 
-    <StatsPanel class="w-[15%]" :tasks="tasks" :times="times" :current-list="currentList" />
-    <Teleport to="body">
-      <VModal :is-open="isOpen" :open-modal="openModal" :close-modal="closeModal">
-        <template #header>
-          <p class="text-black font-medium">
-            Welcome to new template!
-          </p>
-        </template>
-        <template #close>
-          <div class="i-carbon:close close-btn" />
-        </template>
-        <template #content>
-          <div class="bg-emerald-300 p-4 flex flex-col items-center justify-center w-full">
-            <div class="text-white text-4xl">
-              Hello! I'm content
-            </div>
-            <div class="w-1/2 bg-red-300 h-20" />
-          </div>
-        </template>
-      </VModal>
-    </Teleport>
-    <!-- <div>
+        <StatsPanel class="w-[15%]" :tasks="tasks" :times="times" :current-list="currentList" />
+        <Teleport to="body">
+            <VModal :is-open="isOpen" :open-modal="openModal" :close-modal="closeModal">
+                <template #header>
+                    <p class="text-black font-medium">Welcome to new template!</p>
+                </template>
+                <template #close>
+                    <div class="i-carbon:close close-btn" />
+                </template>
+                <template #content>
+                    <div class="bg-emerald-300 p-4 flex flex-col items-center justify-center w-full">
+                        <div class="text-white text-4xl">Hello! I'm content</div>
+                        <div class="w-1/2 bg-red-300 h-20" />
+                    </div>
+                </template>
+            </VModal>
+        </Teleport>
+        <!-- <div>
       <button p-5 text-black bg-indigo-300 @click="openModal">
         OPEN
       </button>
@@ -85,7 +82,7 @@ const { isOpen, openModal, closeModal } = useModal()
         CLOSE
       </button>
     </div> -->
-  </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
@@ -93,11 +90,11 @@ const { isOpen, openModal, closeModal } = useModal()
         @apply bg-no-repeat min-h-screen min-w-full flex p-2;
     }
 
-.black-gradient {
-  background-image: linear-gradient(to right bottom, #000000, #090808, #10100f, #161515, #1a1a19);
-}
+    .black-gradient {
+        background-image: linear-gradient(to right bottom, #000000, #090808, #10100f, #161515, #1a1a19);
+    }
 
-.close-btn {
-  @apply text-black/70 text-6xl duration-300 hover:rotate-[45deg] hover:scale-120 hover:cursor-pointer hover:text-black;
-}
+    .close-btn {
+        @apply text-black/70 text-6xl duration-300 hover:rotate-[45deg] hover:scale-120 hover:cursor-pointer hover:text-black;
+    }
 </style>
