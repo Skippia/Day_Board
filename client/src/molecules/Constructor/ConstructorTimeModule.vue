1f0bcadbc429ea95023997c067f20f5f1af7bf08
Lormida <pocketbook.love24@gmail.com>
2022-07-17 13:32:55 +0300

    feat(scaffold ui constructor template page)

---
+++ client/src/molecules/Constructor/ConstructorTimeModule.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="constructor__time-wrapper">
+        <h1 class="constructor__time-title subtitle">Time configurator</h1>
+
+        <div class="time-configurator p-5">
+            <div class="basis-1/2 flex flex-col">
+                <label class="text-2xl" for="time-separator">Chose time range</label>
+                <select class="time__separator" name="time-separator">
+                    <option disabled default>Time separator</option>
+                    <option value="240">4 hours</option>
+                    <option value="120">2 hours</option>
+                    <option value="60">1 hour</option>
+                </select>
+            </div>
+
+            <div class="flex flex-col">
+                <p class="text-2xl">Chose time range</p>
+                <div class="flex gap-5">
+                    <select class="time__separator" name="time-start">
+                        <option selected="true" disabled>Start time</option>
+                        <option value="240">4 hours</option>
+                    </select>
+                    <select class="time__separator" name="time-end">
+                        <option selected disabled>End time</option>
+                        <option value="240">4 hours</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .constructor__time-wrapper {
+        @apply border-2 p-5;
+    }
+    .constructor__time-title {
+    }
+    .time-configurator {
+        @apply flex justify-between items-center gap-5;
+    }
+    .time__separator {
+        @apply text-white px-3 py-2 text-2xl font-medium text-center bg-neutral-500/20;
+    }
+</style>
---
+++ client/src/pages/constructor/index.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+    <constructor-overlay height="95%">
+        <div class="constructor">
+            <h1 class="constructor__template-title">
+                Hello to creating template calendar
+                <span class="i-carbon:calendar-heat-map text-4xl"></span>
+            </h1>
+            <div class="top-wrapper">
+                <constructor-time-module></constructor-time-module>
+                <button class="button-create-template self-center">Save template</button>
+            </div>
+            <div class="bottom-wrapper">
+                <constructor-tasks-module></constructor-tasks-module>
+            </div>
+        </div>
+    </constructor-overlay>
+</template>
+
+<style lang="scss" scoped>
+    .constructor {
+        @apply p-10 flex flex-col h-full justify-between;
+    }
+    .constructor__template-title {
+        @apply text-4xl flex items-center gap-3;
+    }
+    .top-wrapper {
+        @apply flex justify-around border-2 border-light-100 border-dashed rounded-lg p-5;
+    }
+    .bottom-wrapper {
+    }
+    .button-create-template {
+        background: #2b2d2f;
+        text-align: center;
+        cursor: pointer;
+        padding: 20px 40px;
+        border-radius: 4px;
+        font-weight: bold;
+        font-size: 35px;
+        color: #71dfbe;
+    }
+</style>
