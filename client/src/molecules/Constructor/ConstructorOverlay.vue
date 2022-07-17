a378c67108f4f55e40a3abeac5954a85828ff080
Lormida <pocketbook.love24@gmail.com>
2022-07-17 13:32:55 +0300

    feat(scaffold ui constructor template page)

---
+++ client/src/molecules/Constructor/ConstructorOverlay.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+    const props = defineProps({
+        width: {
+            type: String,
+            default: '90%',
+        },
+        height: {
+            type: String,
+            default: '90%',
+        },
+        bgColor: {
+            type: String,
+            default: 'rgba(22,22,22,0.5)',
+        },
+    })
+</script>
+
+<template>
+    <div class="constructor-overlay">
+        <div class="constructor-overlay__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .constructor-overlay {
+        @apply w-full h-screen flex flex-col items-center justify-center;
+    }
+
+    .constructor-overlay__content {
+        width: v-bind(width);
+        height: v-bind(height);
+        background-color: v-bind(bgColor);
+    }
+</style>
